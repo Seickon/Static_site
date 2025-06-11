cf6666cfb9a4a901b47f6e2164b582f284937ee6
Seickon <simon.eickelschulte@gmx.net>
2025-06-11 19:02:46 +0200

    Wbstite?

--- src/generate_page.py
+++ src/generate_page.py
@@ -2,7 +2,7 @@ import os
 from markdown_to_html import markdown_to_html_node
 from extract_title import extract_title
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     from_file = open(from_path)
     markdown = from_file.read()
@@ -14,20 +14,22 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown) 
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
     path = os.path.dirname(dest_path)
     os.makedirs(path, exist_ok=True)
     file = open(dest_path, "w")
     file.write(html)
     file.close()
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     contets = os.listdir(dir_path_content)
     for dir in contets:
         content = os.path.join(dir_path_content, dir)
         dest = os.path.join(dest_dir_path, dir)
         if os.path.isfile(content):
             dest = dest.replace(".md", ".html")
-            generate_page(content, template_path, dest)
+            generate_page(content, template_path, dest, basepath)
         else:
             os.makedirs(dest, exist_ok=True)
-            generate_pages_recursive(content, template_path, dest)
+            generate_pages_recursive(content, template_path, dest, basepath)
--- src/main.py
+++ src/main.py
@@ -2,13 +2,12 @@ from textnode import *
 import os
 import shutil
 from generate_page import *
+import sys
 
 def main():
-    #test = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    #print(test)
+    basepath = sys.argv
     copy_dictonary()
-    #generate_page(os.path.expanduser("content/index.md"), os.path.expanduser("template.html"), os.path.expanduser("public/index.html"))
-    generate_pages_recursive(os.path.expanduser("content"), os.path.expanduser("template.html"), os.path.expanduser("public"))
+    generate_pages_recursive(os.path.expanduser("content"), os.path.expanduser("template.html"), os.path.expanduser("docs"), basepath)
 
 def rercursion(destination, source):
         contents = os.listdir(source)
@@ -22,7 +21,7 @@ def rercursion(destination, source):
 
 def copy_dictonary():
     source = os.path.expanduser("~/static_site/static")
-    destenation = os.path.expanduser("~/static_site/public")
+    destenation = os.path.expanduser("~/static_site/docs")
     if os.path.exists(destenation):
         shutil.rmtree(destenation)
     os.mkdir(destenation)
